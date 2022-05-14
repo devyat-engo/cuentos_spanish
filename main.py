fb82f2c665dd3656fe6e98c6138af243ecaabbb4
idefix-mac <97406979+devyat-engo@users.noreply.github.com>
2022-05-15 00:21:40 +0200

    main trigger normal update

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
-import re
-from cuento import cuento
+from logs import logging_settings
+from operations import define_cuento, input_handling
 
+
+logger = logging_settings.setup_logger('Main', 'main_log')
 
 # todo: Add the slack push. The idea is to have the /next to call each new sentence
 # todo: Add a menu to choose which story you want to "read"
 # todo: Add a reset counter to 0 controlled by the user
 
 
-def print_per_sentence():
-    with open('counter.txt', 'r') as file:
-        numb = file.readlines()
-        counter = int(numb[0])
-
-    list_sentences = re.split('[.]', cuento)
-    tot_sent = len(list_sentences) - 1
-    list_sentences.pop()
-    if counter < tot_sent:
-        print(f'{list_sentences[counter]}.')
-        counter = counter + 1
-
-        with open('counter.txt', 'w') as f:
-            counter_str = str(counter)
-            f.write(counter_str)
-    else:
-        with open('counter.txt', 'w') as f:
-            counter_str = str(0)
-            f.write(counter_str)
-        print('¡Este cuento se acabó! Si quieres leerlo de nuevo vuelve a escribir ´/next´')
+def main():
+    define_cuento.delete_non_cuentos_files()
+    list_cuentos = define_cuento.get_cuentos()
+    cuento_number = input_handling.input_choose(list_cuentos)
+    define_cuento.set_cuento(cuento_number, list_cuentos)
+    logger.info('Main completed')
 
 
 if __name__ == '__main__':
-    print_per_sentence()
+    main()
