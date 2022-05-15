d583f07c122831c77a8e8c4c9c784dd032760f34
idefix-mac <97406979+devyat-engo@users.noreply.github.com>
2022-05-15 23:06:42 +0200

    adding type_of_read()

--- operations/input_handling.py
+++ operations/input_handling.py
@@ -1,5 +1,6 @@
 
 
+# todo: called from slack with the word "new"
 def input_choose(list_cuentos):
     # 27
     num_cuentos = len(list_cuentos) + 1
@@ -10,6 +11,22 @@ def input_choose(list_cuentos):
             print('The number you choose doesn\'t exist')
         elif cuento_number <= 0:
             print('Negative numbers and 0 are not valid')
+    except:
+        print('Letters and special characters are not permitted')
+
+    return cuento_number
+
+
+# todo: called from slack with the word "read"
+def type_of_read():
+    try:
+        type_read = input(f'Do you want to read a new \"Cuento\" or continue with the previous one? Type either \"new\" or \"continue\" ')
+        cuento_number = int(input(f'Input the number of the \"Cuento\" you want to read: '))
+        if type_read == 'new':
+            print('The number you choose doesn\'t exist')
+        elif type_read == 'continue':
+            print('Negative numbers and 0 are not valid')
+
     except:
         print('Letters and special characters are not permitted')
 
